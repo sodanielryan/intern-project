3dffaefc8e13826fff17eb92d1791814f5ed5724
Daniel Ryan So <sodanielryan@gmail.com>
2026-03-03 18:08:31 +0800

    details-page for pokemon-cards (#4)

--- lib/api.ts
+++ lib/api.ts
@@ -1,21 +1,21 @@
 const BASE_URL = "https://pokeapi.co/api/v2";
 
-export async function getPokemonList(limit = 200, offset = 0) {
+export const getPokemonList = async (limit = 200, offset = 0) => {
   const res = await fetch(
     `${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`,
   );
   const data = await res.json();
   return data.results;
-}
+};
 
-export async function getPokemonDetail(nameOrId: string | number) {
+export const getPokemonDetail = async (nameOrId: string | number) => {
   const res = await fetch(`${BASE_URL}/pokemon/${nameOrId}`);
   const data = await res.json();
   return data;
-}
+};
 
-export async function getPokemonTypes() {
+export const getPokemonTypes = async () => {
   const res = await fetch(`${BASE_URL}/type`);
   const data = await res.json();
   return data.results;
-}
+};
