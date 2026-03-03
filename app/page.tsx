04d282bcf841d91c76c561ab5169c4ec127add5b
sodanielryan <sodanielryan@gmail.com>
2026-03-03 15:57:47 +0800

    List of Pokemon-cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
-import { getPokemonList } from "@/lib/api";
+"use client";
 
-export default async function Home() {
-  const pokemon = await getPokemonList();
-  console.log(pokemon);
+import { useEffect, useState } from "react";
+import { getPokemonDetail, getPokemonList } from "@/lib/api";
+import { Pokemon, PokemonListItem } from "@/types/pokemon";
+import PokemonCard from "@/components/PokemonCard";
+import styles from "@/styles/Home.module.css";
+
+const Home = () => {
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const list = await getPokemonList(100000);
+      const details = await Promise.all(
+        list.map((p: PokemonListItem) => getPokemonDetail(p.name)),
+      );
+      setPokemonList(details);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <main>
-      <h1>PokéDex</h1>
-      <pre>{JSON.stringify(pokemon, null, 2)}</pre>
+    <main className={styles.main}>
+      <h1 className={styles.title}>PokéDex</h1>
+      <div className={styles.grid}>
+        {pokemonList.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
